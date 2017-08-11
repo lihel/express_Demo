75411232dfefb7eb8ccf216fd5e5cfe36a95bd13
Ubuntu <835943102@qq.com>
2017-08-11 11:13:46 +0800

    new

--- app.js
+++ app.js
@@ -13,5 +13,19 @@ app.set('view engine', 'ejs');// 设置模板引擎为 ejs
 app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/users', userRouter);
+app.use("/home", function(request, response, next) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Welcome to the homepage!\n");
+});
+
+app.use("/about", function(request, response, next) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Welcome to the about page!\n");
+});
+
+app.use(function(request, response) {
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.end("404 error!\n");
+});
 
 app.listen(8080);
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.get('/', function(req, res) {
     res.send('hello, express');
 });
 
+
 module.exports = router;
 
